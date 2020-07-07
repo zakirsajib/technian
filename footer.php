8f7611fa8d1987dfe2adbfc8ce8a3e441419bc2d
Zakir Hussain Sajib <zakirsajib@yahoo.co.uk>
2020-07-07 17:59:54 +0100

    first

---
+++ footer.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Technian
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer container">
+		<div class="row">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'technian' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'technian' ), 'WordPress' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'technian' ), 'technian', '<a href="https://zsresume.netlify.app">Zakir Sajib</a>' );
+				?>
+		</div><!-- .site-info -->
+	</div>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
